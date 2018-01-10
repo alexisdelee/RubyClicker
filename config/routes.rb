d1f48d8de34844e451e7f4a4fdbfb82592a23f60
arthur_pechayre <pechay_a@etna-alternance.net>
2018-01-10 23:51:50 +0100

    [fix] Default route and player's id link to game

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "game(/:playerId)" => "game#index", as: :game_frame
+  root "players#index"
+  get "game(/:id)" => "game#index", as: :game_frame
   resources :players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

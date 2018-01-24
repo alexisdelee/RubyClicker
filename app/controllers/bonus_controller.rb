ed6f0f9d1d093c6d00506c26c0529d3725c46874
arthur_pechayre <pechay_a@etna-alternance.net>
2018-01-24 19:59:33 +0100

    [feature] Added controllers for tickers and bonus

--- app/controllers/bonus_controller.rb
+++ app/controllers/bonus_controller.rb
@@ -1,2 +1,38 @@
 class BonusController < ApplicationController
+    before_action :set_bonus, only: [:show, :update, :destroy]
+    skip_before_action :verify_authenticity_token
+
+    def index
+        @bonus = Bonus.all
+        json_response(@bonus)
+    end
+
+    def create
+        @bonus = Bonus.create!(bonus_params)
+        json_response(@bonus, :created)
+    end
+
+    def show
+        json_response(@bonus)
+    end
+
+    def update
+        @bonus.update(bonus_params)
+        head :no_content
+    end
+
+    def destroy
+        @bonus.destroy
+        head :no_content
+    end
+
+    private
+
+    def bonus_params
+        params.permit(:trigger, :text)
+    end
+
+    def set_bonus
+        @bonus = Bonus.find(params[:id])
+    end
 end
--- app/controllers/tickers_controller.rb
+++ app/controllers/tickers_controller.rb
@@ -1,2 +1,38 @@
 class TickersController < ApplicationController
+    before_action :set_ticker, only: [:show, :update, :destroy]
+    skip_before_action :verify_authenticity_token
+
+    def index
+        @tickers = Ticker.all
+        json_response(@tickers)
+    end
+
+    def create
+        @ticker = Ticker.create!(ticker_params)
+        json_response(@ticker, :created)
+    end
+
+    def show
+        json_response(@ticker)
+    end
+
+    def update
+        @ticker.update(ticker_params)
+        head :no_content
+    end
+
+    def destroy
+        @ticker.destroy
+        head :no_content
+    end
+
+    private
+
+    def ticker_params
+        params.permit(:trigger, :text)
+    end
+
+    def set_ticker
+        @ticker = ticker.find(params[:id])
+    end
 end
